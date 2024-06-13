1d8122617e3711be7997e57324c5e0668bfb2a28
Owen Moore <hello@owmo.studio>
2024-06-13 15:44:56 -0700

    ported Timer class

---
+++ src/core/timer.ts
@@ -0,0 +1,71 @@
+export class Timer {
+    #isActive: boolean = false;
+    #previous: number = 0;
+    #current: number = 0;
+    #start: number = 0;
+    #delta: number = 0;
+    #elapsed: number = 0;
+
+    private constructor() {
+        if (typeof document !== 'undefined' && document.hidden !== undefined) {
+            document.addEventListener(
+                'visibilitychange',
+                _ => {
+                    if (document.visibilityState == 'visible') {
+                        this.#current = Timer.now() - this.#start;
+                    }
+                },
+                false,
+            );
+        }
+    }
+
+    public static now = () => (typeof performance === 'undefined' ? Date : performance).now();
+
+    public static create() {
+        return new Timer();
+    }
+
+    public get isActive() {
+        return this.#isActive;
+    }
+
+    public get delta() {
+        return this.#delta / 1000;
+    }
+
+    public get elapsed() {
+        return this.#elapsed / 1000;
+    }
+
+    public start() {
+        if (this.#isActive) return;
+        const now = Timer.now();
+        this.#start = now;
+        this.#current = now - this.#start;
+        this.#isActive = true;
+    }
+
+    public stop() {
+        if (!this.#isActive) return;
+        this.#isActive = false;
+        this.#delta = 0;
+    }
+
+    public reset() {
+        this.#isActive = false;
+        this.#previous = 0;
+        this.#current = 0;
+        this.#start = 0;
+        this.#delta = 0;
+        this.#elapsed = 0;
+    }
+
+    public tick() {
+        if (!this.#isActive) return;
+        this.#previous = this.#current;
+        this.#current = Timer.now() - this.#start;
+        this.#delta = this.#current - this.#previous;
+        this.#elapsed += this.#delta;
+    }
+}
---
+++ tests/timer.test.ts
@@ -0,0 +1,159 @@
+import {Timer} from '../src/core/timer';
+
+describe('Timer', () => {
+    let timer: Timer;
+
+    let nowCalls = 0;
+    let nowMock: jest.SpyInstance<number, []>;
+
+    beforeAll(() => {
+        nowMock = jest.spyOn(Timer, 'now').mockImplementation(() => {
+            nowCalls++;
+            return 1000 * nowCalls;
+        });
+    });
+
+    afterAll(() => {
+        nowMock.mockRestore();
+    });
+
+    test('calling the create() method returns a Timer object', () => {
+        timer = Timer.create();
+        expect(timer).toBeInstanceOf(Timer);
+    });
+
+    test('calling now (mocked) returns a value of 1000', () => {
+        expect(Timer.now()).toBe(1000);
+    });
+
+    describe('after creating a timer', () => {
+        test('isActive should be false', () => {
+            expect(timer.isActive).toBe(false);
+        });
+
+        test('delta should be zero', () => {
+            expect(timer.delta).toBe(0);
+        });
+
+        test('elapsed should be zero', () => {
+            expect(timer.elapsed).toBe(0);
+        });
+
+        test('calling tick() should not change zero values', () => {
+            timer.tick();
+            expect(timer.delta).toBe(0);
+            expect(timer.elapsed).toBe(0);
+        });
+    });
+
+    describe('after calling start()', () => {
+        beforeAll(() => {
+            timer.start();
+        });
+
+        test('isActive should be true', () => {
+            expect(timer.isActive).toBe(true);
+        });
+
+        test('elapsed should be zero', () => {
+            expect(timer.elapsed).toBe(0);
+        });
+
+        test('delta should be zero', () => {
+            expect(timer.delta).toBe(0);
+        });
+    });
+
+    describe('after calling tick()', () => {
+        beforeAll(() => {
+            timer.tick();
+        });
+
+        test('elapsed should be one (1)', () => {
+            expect(timer.elapsed).toBe(1);
+        });
+
+        test('delta should be one (1)', () => {
+            expect(timer.delta).toBe(1);
+        });
+    });
+
+    describe('after calling tick() a second time', () => {
+        beforeAll(() => {
+            timer.tick();
+        });
+
+        test('elapsed should be two (2)', () => {
+            expect(timer.elapsed).toBe(2);
+        });
+
+        test('delta should be one (1)', () => {
+            expect(timer.delta).toBe(1);
+        });
+    });
+
+    describe('after calling stop()', () => {
+        beforeAll(() => {
+            timer.stop();
+        });
+
+        test('isActive should be false', () => {
+            expect(timer.isActive).toBe(false);
+        });
+
+        test('elapsed should be two (2)', () => {
+            expect(timer.elapsed).toBe(2);
+        });
+
+        test('delta should be zero', () => {
+            expect(timer.delta).toBe(0);
+        });
+
+        test('tick() should have no affect on values', () => {
+            timer.tick();
+            expect(timer.isActive).toBe(false);
+            expect(timer.elapsed).toBe(2);
+            expect(timer.delta).toBe(0);
+        });
+    });
+
+    describe('callilng start() after stop(), followed by two tick() calls', () => {
+        beforeAll(() => {
+            timer.start();
+            timer.tick();
+            timer.tick();
+        });
+
+        test('elapsed should be three (4)', () => {
+            expect(timer.elapsed).toBe(4);
+        });
+
+        test('delta should be one (1)', () => {
+            expect(timer.delta).toBe(1);
+        });
+    });
+
+    describe('calling reset()', () => {
+        beforeAll(() => {
+            timer.reset();
+        });
+
+        test('isActive should be false', () => {
+            expect(timer.isActive).toBe(false);
+        });
+
+        test('delta should be zero', () => {
+            expect(timer.delta).toBe(0);
+        });
+
+        test('elapsed should be zero', () => {
+            expect(timer.elapsed).toBe(0);
+        });
+
+        test('calling tick() should not change zero values', () => {
+            timer.tick();
+            expect(timer.delta).toBe(0);
+            expect(timer.elapsed).toBe(0);
+        });
+    });
+});
