3a70754c470df9b751630b1da36494402626c607
Owen Moore <hello@owmo.studio>
2024-06-21 19:06:53 -0700

    bump v0.4.1 - added exports for Pipeline and Node

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,7 @@ export {Random} from './core/random';
 
 export {InputsHandler} from './interaction/inputs-handler';
 
+export {Pipeline} from './pipeline/pipeline';
+export {Node} from './pipeline/node';
+
 export {Utils} from './helpers/utils';
